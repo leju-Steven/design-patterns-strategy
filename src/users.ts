c1566cc91debbcaf6a7bba485d5a03e9a3827027
leju-Steven <steven@leju.com.tw>
2025-01-21 21:35:51 +0800

    feat: create users function

---
+++ src/users.ts
@@ -0,0 +1,42 @@
+import { Individual } from "./class/Individual";
+
+function generateRandomUsers(): Individual[] {
+  const genders = ["male", "female"];
+  const habitsList = [
+    "reading",
+    "swimming",
+    "coding",
+    "hiking",
+    "gaming",
+    "cooking",
+    "traveling",
+    "dancing",
+    "singing",
+    "painting",
+  ];
+
+  const getRandomElement = (arr: string[]) =>
+    arr[Math.floor(Math.random() * arr.length)];
+  const getRandomNumber = (min: number, max: number) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const users: Individual[] = [];
+  for (let i = 1; i <= 10; i++) {
+    const id = i;
+    const gender = getRandomElement(genders);
+    const age = getRandomNumber(18, 60);
+    const intro = "This is a random intro.";
+    const habits = Array.from({ length: getRandomNumber(1, 5) }, () =>
+      getRandomElement(habitsList)
+    );
+    const coord = [getRandomNumber(0, 90), getRandomNumber(0, 180)];
+
+    users.push(new Individual(id, gender, age, intro, habits, coord));
+  }
+
+  return users;
+}
+
+const users = generateRandomUsers();
+
+export { users };
